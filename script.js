e1e107602c38945574a31baf736b7a3598d37702
xdcycloid <achintya623@gmail.com>
2024-10-05 13:59:05 +0530

    added grids fix event

---
+++ script.js
@@ -0,0 +1,21 @@
+const container= document.querySelector(".container");
+// for(x=0;x<256;x++){
+//     const gridBox= document.createElement("div");
+//     gridBox.className="gridElement";
+//     container.append(gridBox);
+// }
+let gridBox;
+for(x=0;x<16;x++){
+    const gridRow= document.createElement("div");
+    gridRow.className=`gridrow${x}`
+    container.append(gridRow);
+    for(y=0;y<16;y++){
+         gridBox= document.createElement("div");
+        gridBox.className="gridElement";
+        gridRow.append(gridBox);
+    }
+}
+gridBox= document.querySelector(".gridElement");
+gridBox.addEventListener("click", function(e){
+    e.target.style.backgroundColor="blue";
+});
